3f267fb1bf81de4d02708194211861254b7ed300
mavensingh <kuldeep.avsar@gmail.com>
2020-06-20 13:35:23 +0530

    rotating array values

---
+++ All-languages/array/array_rotation/rotate.py
@@ -0,0 +1,71 @@
+# # Python program to rotate an array
+# def LeftRotate(arr, d, n):
+#     for i in range(d):
+#         leftRotatebyOne(arr, n)
+
+
+# def leftRotatebyOne(arr, n):
+#     temp = arr[0]
+#     for i in range(n-1):
+#         arr[i] = arr[i+1]
+#     arr[n-1] = temp
+#     print(temp)
+
+
+# def printArray(arr, size):
+#     for i in range(size):
+#         print(arr[i])
+
+
+# arr = [1, 2, 3, 4, 5, 6, 7]
+# LeftRotate(arr, 2, 7)
+# printArray(arr, 7)
+
+# Juggling Algorithm
+
+
+# Python3 program to rotate an array by
+# d elements
+# Function to left rotate arr[] of size n by d
+def leftRotate(arr, d, n):
+    d = d % n
+    g_c_d = gcd(d, n)
+    for i in range(g_c_d):
+
+        # move i-th values of blocks
+        temp = arr[i]
+        j = i
+        while 1:
+            k = j + d
+            if k >= n:
+                k = k - n
+            if k == i:
+                break
+            arr[j] = arr[k]
+            j = k
+        arr[j] = temp
+
+# UTILITY FUNCTIONS
+# function to print an array
+
+
+def printArray(arr, size):
+    for i in range(size):
+        print("% d" % arr[i], end=" ")
+
+# Fuction to get gcd of a and b
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+# Driver program to test above functions
+arr = [1, 2, 3, 4, 5, 6, 7]
+n = len(arr)
+d = 2
+leftRotate(arr, d, n)
+printArray(arr, n)
